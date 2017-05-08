1a0a0b56d2a1dbaae016dab0ba7cc0ed3b38bac4
Rasmus.Iila <rasmus.iila@ttu.ee>
2017-05-08 16:44:10 +0100

    added shel scripts

---
+++ shell_scripts/script_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo ".."
+echo ".."
+echo ".."
+sleep 10s # It's not crucial to have the waiting time but just so the student has time to get ready to watch
+echo "$1"
+
+directory_name="$1"
+
+python3 "$directory_name.py"
